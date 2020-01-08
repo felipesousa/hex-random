40dd6a7ebac602f9d80945177d4ddac85c3bd76d
Felipe Sousa <felipzbr@gmail.com>
2020-01-07 23:58:27 -0300

    [feature]: define class and onInit methods

--- app.ts
+++ app.ts
@@ -1,4 +1,24 @@
+class App {
+    el: Element
+
+    constructor(el: Element) {
+        this.el = el
+    }
+
+    onInit(): void {
+        setInterval(() => {
+            if (this.el) {
+                this.el.innerHTML = `#${Math.floor(Math.random()*16777215).toString(16)}`
+            }
+        }, 1000)
+    }
+}
+
 function main() {
     const app = document.querySelector('#app')
-    app.innerHTML = 'WORKS'
+
+    if (app) {
+        const instance = new App(app)
+        instance.onInit()
+    }
 }
